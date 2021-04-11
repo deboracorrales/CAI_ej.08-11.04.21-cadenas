d93f87111432243f93d4c7e5778742d90ed42676
deboracorrales <deboramailen88@gmail.com>
2021-04-11 01:49:29 -0300

    Agregar archivos de proyecto.

---
+++ CAI_ej.08-03.04.21-cadenas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_ej._08_03._04._21_cadenas
+//ej 08: Solicite al usuario el ingreso de una frase y presente un mensaje indicando si alguna 
+//contiene la palabra “fin”.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string texto;
+			string palabra = "fin";
+			//bool contiene = false;
+
+
+			Console.WriteLine("Ingrese una frase");
+			texto = Console.ReadLine();
+
+			if ((texto.Contains(palabra)) == true)
+			{
+				Console.WriteLine("Bien! su frase contiene la palabra " + palabra);
+			}
+
+			Console.WriteLine("Adios!");
+
+			Console.ReadKey();
+			
+		}
+    }
+}
